02e0037fcf671a71dfff825bae393e72b7977c53
birdplanedinosaur <Shirtlessbear01>
2018-08-02 08:39:39 -0500

    proof of concept

---
+++ src/com/example/war/War.java
@@ -0,0 +1,9 @@
+package com.example.war;
+
+public class War {
+    public static void main (String[] args) {
+        System.out.println("Hello commit Warrior 2.0");
+    }
+}
+
+/* * new note to test version control */
